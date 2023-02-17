f4b2892ef5a051f7181cf5b0aa1ca66e55617f20
Arthur Obo-Nwakaji <arthuron57@gmail.com>
2023-02-17 16:28:22 +0100

    Added hx-vals which supports django ckeditor to post data successfully

--- platform_admin/models.py
+++ platform_admin/models.py
@@ -44,7 +44,7 @@ class MarketSector(models.Model):
     clan = models.ForeignKey("utility.Clan", blank=True, null=True, on_delete=models.SET_NULL)
     address_location = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=255)
-    description = RichTextUploadingField(blank=True, null=True,)
+    description = RichTextUploadingField()
     date_created = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
 
