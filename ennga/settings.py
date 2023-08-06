2f4cec1661849d130d2b5e8303c321279eb7013b
arthur <sph@sphs-MacBook-Air.local>
2023-08-06 19:02:56 +0100

    Updated settings.py to production settings

--- ennga/settings.py
+++ ennga/settings.py
@@ -15,8 +15,8 @@ from datetime import timedelta
 from decouple import config
 from pathlib import Path
 
-# import django_heroku
-# import dj_database_url
+import django_heroku
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -100,27 +100,27 @@ WSGI_APPLICATION = 'ennga.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# # Production Database
-# DB_NAME = config("DB_NAME")
-# DB_USER = config("DB_USER")
-# DB_PASSWORD = config("DB_PASSWORD")
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': DB_NAME,
-#         'USER': DB_USER,
-#         'PASSWORD': DB_PASSWORD,
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+# Production Database
+DB_NAME = config("DB_NAME")
+DB_USER = config("DB_USER")
+DB_PASSWORD = config("DB_PASSWORD")
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
